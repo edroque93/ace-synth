2708721b505e572397ad16172aa754fc838b9274
cabul <calvin.bulla@gmail.com>
2016-05-09 18:33:07 +0200

    Modified jtag preparation tool

--- tools/jtag.sh
+++ tools/jtag.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
+
+QUARTUS_HOME=${QUARTUS_HOME:-/opt/altera/q2web/quartus}
+
 sudo killall jtagd
 sudo chmod 755 /sys/kernel/debug/usb/devices
 sudo chmod 755 /sys/kernel/debug/usb
 sudo chmod 755 /sys/kernel/debug
 sudo mount --bind /dev/bus /proc/bus
 sudo ln -s /sys/kernel/debug/usb/devices /proc/bus/usb/devices
-sudo /opt/altera/q2web/quartus/bin/jtagd
-sudo /opt/altera/q2web/quartus/bin/jtagconfig
+sudo $QUARTUS_HOME/bin/jtagd
+sudo $QUARTUS_HOME/bin/jtagconfig
