d678b22a655b0f629f23c1cfff091afcc9f02100
barredator <adrian.barredof@gmail.com>
2016-05-22 17:40:49 +0200

    IF WB + verification

--- src/if_stage.v
+++ src/if_stage.v
@@ -1,7 +1,6 @@
 module if_stage (
 	input wire clk,
 	input wire reset,
-	input wire we,
 	// Control signals
 	// - Flow
 	input wire pc_reset,
@@ -16,12 +15,68 @@ module if_stage (
 	// - Arbiter Control
 	output reg read_req,
 	input wire read_ack,
-	output reg [31:0] read_addr,
-	input wire [31:0] read_data,	
+	output reg [31:0] read_addr=0,
+	input wire [31:0] read_data,
 	// Outputs
-	output reg [31:0] instruction,
-	output reg [31:0] pc_next,
-	output reg hit
+	output reg [31:0] instruction=0,
+	output reg [31:0] pc_next=0,
+	output reg hit=0
 );
+
+reg [31:0] pc_interm, pc_real, pc_now, pc_next_next;
+reg state, state_next;
+reg read_req_next;
+reg [31:0] instruction_next;
+reg hit_next;
+
+always @(posedge clk) begin
+   if (reset) begin
+      state <= state_idle;
+      read_req <= 0;
+      read_addr <= 32'd0;
+      instruction <= 32'd0;
+      pc_next <= 32'd0;
+      hit <= 0;
+   end else begin
+      state <= state_next;
+      read_req <= read_req_next;
+      read_addr <= pc_next_next;
+		pc_next <= pc_next_next + 4;
+      instruction <= instruction_next;
+      hit <= hit_next;
+   end
+end
+
+// State machine
+localparam state_idle = 0, state_read = 1;
+
+always @* begin
+   state_next   = state_idle;
+   read_req_next = 1'b0;
+   hit_next = 1'b0;
+
+  case (state)
+    state_idle: begin
+       state_next = state_read;
+       if (pc_reset) begin
+	       pc_next_next = 32'd0;
+	    end else if(pc_we) begin
+		    pc_now = pc_next + 4;
+	       pc_interm = is_jump ? jump_addr : pc_now;
+	       pc_next_next = is_branch ? branch_addr : pc_interm;
+       end
+       read_req_next = 1'b1;
+    end
+    state_read: begin
+       state_next = state_read;
+       if(read_ack) begin
+	       instruction_next = read_data;
+	       read_req_next = 1'b0;
+	       state_next = state_idle;
+	       hit_next = 1'b1;
+      end
+    end
+  endcase
+end
 
 endmodule
--- src/wb_stage.v
+++ src/wb_stage.v
@@ -1,12 +1,11 @@
 module wb_stage (
 	input wire clk,
 	input wire reset,
-	input wire we,
 	// Control signals
-	// - Forward 
-	output wire [4:0] reg_probe,
-	output wire [31:0] data_probe,
-	output wire write_probe,
+	// - Forward
+	output reg [4:0] reg_probe,
+	output reg [31:0] data_probe,
+	output reg write_probe,
 	// Inputs
 	input wire [31:0] reg_data,
 	input wire [4:0] reg_addr,
@@ -14,6 +13,24 @@ module wb_stage (
 	// Feedback
 	output reg [31:0] reg_data_out,
 	output reg [4:0] reg_addr_out,
-	output reg reg_write_out,
+	output reg reg_write_out
 );
+
+always @(posedge clk) begin
+   if(reset) begin
+      reg_probe <= 5'd0;
+      data_probe <= 32'd0;
+      write_probe <= 0;
+      reg_data_out <= 32'd0;
+      reg_addr_out <= 5'd0;
+      reg_write_out <= 0;
+   end else
+      reg_probe <= reg_addr_out;
+      data_probe <= reg_data;
+      write_probe <= reg_write;
+      reg_data_out <= reg_data;
+      reg_addr_out <= reg_addr_out;
+      reg_write_out <= reg_write;
+end
+
 endmodule
