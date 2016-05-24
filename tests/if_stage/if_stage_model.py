2824fc79ba651f9fe64d4de553bfda6e0241222e
barredator <adrian.barredof@gmail.com>
2016-05-24 18:51:08 +0200

    If_stage verification working! Calvin <3

---
+++ tests/if_stage/if_stage_model.py
@@ -0,0 +1,6 @@
+def if_stage_model(clk, reset, we, pc_reset, pc_we, is_jump, is_branch, jump_addr, branch_addr, read_ack, read_data):
+    """ if_stage_model """
+
+
+
+    return (instruction, pc_next, hit)
